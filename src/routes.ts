547902a156e47a1f38ff74c80383f246daf43c8f
Richard-S-Rodrigues <richardsouzarodrigues555@gmail.com>
2021-10-19 18:39:47 -0300

    Stage 1

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,18 @@
 import { Router } from "express";
 import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
+import { CreateMessageController } from "./controllers/CreateMessageController";
+import { GetLast3MessagesController } from "./controllers/GetLast3MessagesController";
+import { UserProfileController } from "./controllers/UserProfileController";
+import { auth } from "./middlewares/auth";
 
 const router = Router();
 
 router.post("/authenticate", new AuthenticateUserController().handle);
+
+router.post("/messages", auth, new CreateMessageController().handle);
+
+router.get("/messages/last3", new GetLast3MessagesController().handle);
+
+router.get("/profile", auth, new UserProfileController().handle);
 
 export { router };
